260011bcaab0ea23fc20d06285907682c78a7b1c
Neko <software@skneko.moe>
2022-07-10 21:06:54 +0200

    Add download button in results step

--- web/src/pages/ResultStepPage.svelte
+++ web/src/pages/ResultStepPage.svelte
@@ -5,11 +5,14 @@
     import WizardStepLayout from "@layouts/WizardStepLayout.svelte";
     import { rootPath } from "@stores/context";
     import {
-        addendumModuleName,
-        constraints,predicatesAddendum,program,
-        rootModuleName
+    addendumModuleName,
+    constraints,
+    predicatesAddendum,
+    program,
+    rootModuleName
     } from "@stores/wizardSession";
     import { Strass } from "api/strass";
+    import FileEarkmarkArrowDown from "svelte-bootstrap-icons/lib/FileEarmarkArrowDown/FileEarmarkArrowDown.svelte";
     import { navigate } from "svelte-routing";
 
     const wizardStepProps = {
@@ -30,6 +33,16 @@
         addendumModuleName: $addendumModuleName,
         constraints: $constraints,
     });
+
+    function download(filename: string, text: string) {
+        var link = document.createElement("a");
+        link.setAttribute(
+            "href",
+            "data:text/plain;charset=utf-8," + encodeURIComponent(text)
+        );
+        link.setAttribute("download", filename);
+        link.click();
+    }
 </script>
 
 <WizardStepLayout {...wizardStepProps}>
@@ -57,4 +70,22 @@
         <Alert title="Internal error">Please report this error.</Alert>
         {e}
     {/await}
+
+    <div slot="bottom-stripe-right">
+        {#await fixRequest then fixed}
+            {#if fixed.success}
+                <button
+                    type="button"
+                    class="btn btn-primary"
+                    on:click={() =>
+                        download(
+                            `${$rootModuleName.toLowerCase()}_fixed.maude`,
+                            `${$program}\n\n${fixed.result}`
+                        )}
+                >
+                    <FileEarkmarkArrowDown /> Download fixed program
+                </button>
+            {/if}
+        {/await}
+    </div>
 </WizardStepLayout>
